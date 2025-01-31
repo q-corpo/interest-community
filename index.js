97568f01fb46b339b58dde2183c5827cc237dcc5
birthofq <birthofq@gmail.com>
2025-01-31 21:45:32 +0100

    add test function for generating oauth nonce

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+import crypto from 'crypto';
 dotenv.config()
 const app = express();
 app.use(express.json());
 const PORT = process.env.PORT;
+
+const oauth_nonce = crypto.randomBytes(32).toString("base64").replace(/\W/g, "");
+console.log(oauth_nonce);
 
 app.listen(PORT, (req, res)=> {
   console.log('server is up')
@@ -12,3 +16,6 @@ app.listen(PORT, (req, res)=> {
 app.get('/', (req, res)=>{
   res.send('Welcome to the interest community web app')
 })
+
+
+
