a56d4eb439a6d191346258233db73544a31d90ea
birthofq <birthofq@gmail.com>
2025-02-20 14:45:47 +0100

    begin setup of twitter auth server

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import crypto from 'crypto';
+
 dotenv.config()
 const app = express();
 app.use(express.json());
 const PORT = process.env.PORT;
-
-const oauth_nonce = crypto.randomBytes(32).toString("base64").replace(/\W/g, "");
-console.log(oauth_nonce);
 
 app.listen(PORT, (req, res)=> {
   console.log('server is up')
@@ -16,6 +13,7 @@ app.listen(PORT, (req, res)=> {
 app.get('/', (req, res)=>{
   res.send('Welcome to the interest community web app')
 })
+
 
 
 
